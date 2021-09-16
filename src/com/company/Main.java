5703a2e60e02430c65c988f214ec4c19892aefe0
liguoyan <liguoyan@meituan.com>
2021-09-16 14:27:16 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 0; i < 4; i++) {
+            System.out.println("hello world: " +i);
+        }
+    }
+}
